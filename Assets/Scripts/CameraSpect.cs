86a82ad2f3e71118237235bbf0554f0bcf203457
kirbyzhou <kirbyzhou@protonmail.com>
2020-07-26 18:25:34 -0700

    Add scene for demo

--- Assets/Scripts/CameraSpect.cs
+++ Assets/Scripts/CameraSpect.cs
@@ -7,7 +7,7 @@ public class CameraSpect : MonoBehaviour
 {
     public bool cameraMovementActivated = false;
     public float moveSpeed;
-    public float shiftAdditionalSpeed;
+    // public float shiftAdditionalSpeed;
     public float mouseSensitivity;
     public bool invertMouse;
     public bool autoLockCursor;
@@ -23,27 +23,27 @@ public class CameraSpect : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetMouseButtonDown(1))
         {
-            cameraMovementActivated != cameraMovementActivated;
+            cameraMovementActivated = !cameraMovementActivated;
         }
         if (cameraMovementActivated)
         {
-            float speed = (moveSpeed + (Input.GetAxis("Fire3") * shiftAdditionalSpeed));
-            this.gameObject.transform.Translate(Vector3.forward * speed * Input.GetAxis("Vertical"));
-            this.gameObject.transform.Translate(Vector3.right * speed * Input.GetAxis("Horizontal"));
-            this.gameObject.transform.Translate(Vector3.up * speed * (Input.GetAxis("Jump") + (Input.GetAxis("Fire1") * -1)));
+            // float speed = (moveSpeed + (Input.GetAxis("Fire3") * shiftAdditionalSpeed));
+            this.gameObject.transform.Translate(Vector3.forward * moveSpeed * Input.GetAxis("Vertical"));
+            this.gameObject.transform.Translate(Vector3.right * moveSpeed * Input.GetAxis("Horizontal"));
+            // this.gameObject.transform.Translate(Vector3.up * speed * (Input.GetAxis("Jump") + (Input.GetAxis("Fire1") * -1)));
             this.gameObject.transform.Rotate(Input.GetAxis("Mouse Y") * mouseSensitivity * ((invertMouse) ? 1 : -1), Input.GetAxis("Mouse X") * mouseSensitivity * ((invertMouse) ? -1 : 1), 0);
             this.gameObject.transform.localEulerAngles = new Vector3(this.gameObject.transform.localEulerAngles.x, this.gameObject.transform.localEulerAngles.y, 0);
 
-            if (Cursor.lockState == CursorLockMode.None && Input.GetMouseButtonDown(0))
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-            }
-            else if (Cursor.lockState == CursorLockMode.Locked && Input.GetKeyDown(KeyCode.Escape))
-            {
-                Cursor.lockState = CursorLockMode.None;
-            }
+            // if (Cursor.lockState == CursorLockMode.None && Input.GetMouseButtonDown(0))
+            // {
+            //     Cursor.lockState = CursorLockMode.Locked;
+            // }
+            // else if (Cursor.lockState == CursorLockMode.Locked && Input.GetKeyDown(KeyCode.Escape))
+            // {
+            //     Cursor.lockState = CursorLockMode.None;
+            // }
         }
     }
 }
--- Assets/Scripts/GlobalManager.cs
+++ Assets/Scripts/GlobalManager.cs
@@ -1,18 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Parabox.CSG;
 
 public class GlobalManager : MonoBehaviour
 {
+    public GameObject scannedSample;
+    public GameObject tool;
+    private Vector3 failureRaycastPoint;
     // Start is called before the first frame update
     void Start()
     {
-
+        failureRaycastPoint = new Vector3(0.0f, 0.0f, 0.0f);
+        tool.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 toolPos = getHitPoint();
+        if (toolPos != failureRaycastPoint)
+        {
+            tool.transform.position = toolPos;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            scannedSample = ReturnClickedObject(out hitInfo);
+            if (scannedSample != null)
+            {
+                CSG_Model result = subtraction(scannedSample, tool);
+                scannedSample.GetComponent<MeshFilter>().sharedMesh = result.mesh;
+                scannedSample.GetComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+                // composite = new GameObject();
+                // composite.AddComponent<MeshFilter>().sharedMesh = result.mesh;
+                // composite.AddComponent<MeshRenderer>().sharedMaterials = result.materials.ToArray();
+            }
+        }
+    }
 
+    GameObject ReturnClickedObject(out RaycastHit hit)
+    {
+        GameObject targetObject = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            targetObject = hit.collider.gameObject;
+        }
+        return targetObject;
+    }
+
+    Vector3 getHitPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        Debug.Log("Hit" + hit.point);
+        return hit.point;
+    }
+
+    CSG_Model subtraction(GameObject scannedSample, GameObject tool)
+    {
+        return Boolean.Subtract(scannedSample, tool);
+    }
+
+    void setToolObject(GameObject toolObject)
+    {
+        tool = toolObject;
     }
 }
